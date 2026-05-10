ca9343a1b4826c675fb96108d9225d727e5bf97e
Gian <58370608+glpecile@users.noreply.github.com>
2026-05-10 11:03:08 -0300

    fix: env.d.ts wrong typings

--- src/env.d.ts
+++ src/env.d.ts
@@ -13,26 +13,6 @@ declare module "satori/yoga.wasm" {
 	export default module;
 }
 
-declare module "*.wasm?module" {
-	const module: WebAssembly.Module;
-	export default module;
-}
-
-declare module "*.woff?url" {
-	const url: string;
-	export default url;
-}
-
-declare module "*.woff2?url" {
-	const url: string;
-	export default url;
-}
-
-declare module "*.png?url" {
-	const url: string;
-	export default url;
-}
-
 interface Window {
 	__glpecileTheme: {
 		storageKey: string;
