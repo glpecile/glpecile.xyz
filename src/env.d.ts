a3c0d17b9fd72650b0837e834e2eec2781f00980
Gian <58370608+glpecile@users.noreply.github.com>
2026-05-10 10:57:03 -0300

    feat(footer): add theme switcher

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,16 @@ declare module "*.wasm" {
 	export default module;
 }
 
+declare module "@resvg/resvg-wasm/index_bg.wasm" {
+	const module: WebAssembly.Module;
+	export default module;
+}
+
+declare module "satori/yoga.wasm" {
+	const module: WebAssembly.Module;
+	export default module;
+}
+
 declare module "*.wasm?module" {
 	const module: WebAssembly.Module;
 	export default module;
@@ -21,4 +31,15 @@ declare module "*.woff2?url" {
 declare module "*.png?url" {
 	const url: string;
 	export default url;
+}
+
+interface Window {
+	__glpecileTheme: {
+		storageKey: string;
+		root: HTMLElement;
+		media: MediaQueryList;
+		getSystemTheme(): "light" | "dark";
+		apply(theme: "auto" | "light" | "dark"): void;
+		persist(theme: "auto" | "light" | "dark"): void;
+	};
 }
